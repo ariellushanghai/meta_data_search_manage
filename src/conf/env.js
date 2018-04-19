ae279ee8ad838a866402149ce8fba0fccaf68527
LUHUAN <luhuan994@pingan.com.cn>
2018-04-19 16:28:11 +0800

    Prepare for test

--- src/conf/env.js
+++ src/conf/env.js
@@ -12,9 +12,11 @@ if (process.env.NODE_ENV === "development") {
   baseURL = "http://localhost:8080/api";
   // baseURL = 'http://localhost:8080'
 } else {
-  redirectURL = "http://bip.pab.com.cn/cm/login.html?redirectUrl=" + redirect_uri + "";
-  loginURL = "http://bip.pab.com.cn/cm/login.html?redirectUrl=";
-  baseURL = "http://bip.pab.com.cn/cm/data-manager-svc/v1/dspmonitor";
+  // redirectURL = "http://bip.pab.com.cn/cm/login.html?redirectUrl=" + redirect_uri + "";
+  // loginURL = "http://bip.pab.com.cn/cm/login.html?redirectUrl=";
+  redirectURL = "http://10.14.220.124:8080/cm/login.html?redirectUrl=" + redirect_uri + "";
+  loginURL = "http://10.14.220.124:8080/cm/login.html?redirectUrl=";
+  baseURL = "http://10.14.224.180:9090/api";
 }
 
 export { redirectURL, loginURL, baseURL };
